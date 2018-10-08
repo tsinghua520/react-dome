ef08480596704cd047e0a0ae0e2e6afd132d8906
tsinghua <v-zhuqinghua@huazhu.com>
2018-10-08 11:52:41 +0800

    iconfont

--- src/App/components/My/Center.js
+++ src/App/components/My/Center.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import '../../../static/font/iconfont.css';
+import '../../../static/css/center.css'
 class App extends React.Component {
   render() {
     return (
       <div>
-        个人中心
+        <div>
+          <span className="iconfont icon-photo photo"></span>
+          <span>myusername</span>
+        </div>
+        <ul>
+          <li>
+            <i className="iconfont icon-order"></i>
+            <p>我的预订</p>
+          </li>
+          <li>
+            <i className="iconfont icon-order"></i>
+            <p>我的预订</p>
+          </li>
+          <li>
+            <i className="iconfont icon-order"></i>
+            <p>我的预订</p>
+          </li>
+          <li>
+            <i className="iconfont icon-order"></i>
+            <p>我的预订</p>
+          </li>
+        </ul>
       </div>
     )
   }
