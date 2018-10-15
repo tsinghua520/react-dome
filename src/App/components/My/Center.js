3c086df14b21fef178dd64091c59736871b5cc50
tsinghua <v-zhuqinghua@huazhu.com>
2018-10-15 15:52:52 +0800

    add

--- src/App/components/My/Center.js
+++ src/App/components/My/Center.js
@@ -4,27 +4,47 @@ import '../../../static/css/center.css'
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="mycenter">
         <div>
           <span className="iconfont icon-photo photo"></span>
           <span>myusername</span>
         </div>
         <ul>
           <li>
-            <i className="iconfont icon-order"></i>
-            <p>我的预订</p>
+            <a>
+              <i className="iconfont icon-book"></i>
+              <p>我的预订</p>
+            </a>
           </li>
           <li>
-            <i className="iconfont icon-order"></i>
-            <p>我的预订</p>
+            <a>
+              <i className="iconfont icon-order"></i>
+              <p>订单列表</p>
+            </a>
           </li>
           <li>
-            <i className="iconfont icon-order"></i>
-            <p>我的预订</p>
+            <a>
+              <i className="iconfont icon-bill"></i>
+              <p>我的账单</p>
+            </a>
           </li>
           <li>
-            <i className="iconfont icon-order"></i>
-            <p>我的预订</p>
+            <a>
+              <i className="iconfont icon-cardbag"></i>
+              <p>我的卡包</p>
+            </a>
+          </li>
+          <li>
+            <a>
+              <i className="iconfont icon-invoice"></i>
+              <p>我的发票</p>
+            </a>
+          </li>
+          <li>
+            <a>
+              <i className="iconfont icon-activity"></i>
+              <p>我的活动</p>
+            </a>
           </li>
         </ul>
       </div>
